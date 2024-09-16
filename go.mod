25a233bb5fdf65c680d64ac145d29a914c7ed4c1
sara <49288363+sarahrashidi@users.noreply.github.com>
2024-09-16 19:31:14 +0330

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module investment.go
+
+go 1.22.5
---
+++ investment.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	const inflationRate = 6.5
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("expectedReturnRate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
